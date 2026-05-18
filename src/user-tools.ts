eec9371121710c1aeaf06de3631c60939415c1c6
Aries <me@ariesclark.com>
2026-05-18 00:05:53 -0400

    fix: drop vrcx profile link

--- src/user-tools.ts
+++ src/user-tools.ts
@@ -8,11 +8,4 @@ if (mw.util.isPortletVisible(portletId)) {
 	const userId = lowercaseFirst(mw.config.get("wgPageName").split(":")[1]!);
 
 	mw.util.addPortletLink(portletId, `https://vrchat.com/home/user/${userId}`, "VRChat profile");
-
-	if (mw.config.get("wgUserGroups")?.includes("community-mod"))
-		mw.util
-			.addPortletLink(portletId, `vrcx://user/${userId}`, "VRCX profile")!
-			.querySelector("a")!
-			.classList
-			.add("external");
 }
