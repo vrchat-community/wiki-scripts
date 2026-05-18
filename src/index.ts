07f619785b300a612cea03aa23210916324b8e5f
Aries Clark <me@ariesclark.com>
2026-05-18 00:34:58 -0400

    test: show commit messages in ci

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ import "./themes/any/index.scss";
 import "./templates";
 import "./plausible";
 import "./user-tools";
+
+// eslint-disable-next-line no-console
+console.log("Wiki scripts loaded.");
